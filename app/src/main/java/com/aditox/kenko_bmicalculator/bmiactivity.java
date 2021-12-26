8b1364ad50db58a12653f9fda4b2635b80e0aa2b
Aditya <adityasahu30082@gmail.com>
2021-12-26 22:01:55 +0530

    BMI Calculator App

---
+++ app/src/main/java/com/aditox/kenko_bmicalculator/bmiactivity.java
@@ -0,0 +1,125 @@
+package com.aditox.kenko_bmicalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.text.Html;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+public class bmiactivity extends AppCompatActivity {
+
+    android.widget.Button mrecalculatebmi;
+
+    TextView mbmidisplay,magedisplay,mweightdisplay,mheightdisplay,mbmicategory,mgender;
+    Button mgotomain;
+    Intent intent;
+    ImageView mimageview;
+    String mbmi;
+    String cateogory;
+    float intbmi;
+
+    String height;
+    String weight;
+
+    float intheight,intweight;
+
+    RelativeLayout mbackground;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bmiactivity);
+        getSupportActionBar().setElevation(0);
+        getSupportActionBar().setTitle(Html.fromHtml("<font color=\"white\"></font>"));
+        getSupportActionBar().setTitle("Result");
+        ColorDrawable colorDrawable=new ColorDrawable(Color.parseColor("#2f3542"));
+        getSupportActionBar().setBackgroundDrawable(colorDrawable);
+
+        intent=getIntent();
+        mbmidisplay=findViewById(R.id.bmidisplay);
+        mbmicategory = findViewById(R.id.bmicategorydispaly);
+        mgender = findViewById(R.id.genderdisplay);
+        mbackground = findViewById(R.id.contentlayout);
+        mimageview = findViewById(R.id.imageview);
+        mrecalculatebmi = findViewById(R.id.recalculatebmi);
+
+        height=intent.getStringExtra("height");
+        weight=intent.getStringExtra("weight");
+
+        intheight=Float.parseFloat(height);
+        intweight=Float.parseFloat(weight);
+
+        intheight=intheight/100;
+        intbmi=intweight/(intheight*intheight);
+
+
+        mbmi=Float.toString(intbmi);
+
+        if(intbmi<16)
+        {
+            mbmicategory.setText("Severe Thinness");
+            mbackground.setBackgroundColor(Color.RED);
+            mimageview.setImageResource(R.drawable.cross);
+        }
+        else if(intbmi<16.9 && intbmi>16)
+        {
+            mbmicategory.setText("Moderate Thinness");
+            mbackground.setBackgroundColor(Color.RED);
+            mimageview.setImageResource(R.drawable.warning);
+        }
+        else if(intbmi<18.4 && intbmi>17)
+        {
+            mbmicategory.setText("Mild Thinness");
+            mbackground.setBackgroundColor(Color.YELLOW);
+            mimageview.setImageResource(R.drawable.warning);
+            //   mimageview.setBackground(colorDrawable2);
+        }
+        else if(intbmi<24.9 && intbmi>18.5 )
+        {
+            mbmicategory.setText("Normal");
+            mimageview.setImageResource(R.drawable.ok);
+        }
+        else if(intbmi <29.9 && intbmi>25)
+        {
+            mbmicategory.setText("Overweight");
+            mbackground.setBackgroundColor(Color.YELLOW);
+            mimageview.setImageResource(R.drawable.warning);
+        }
+        else if(intbmi<34.9 && intbmi>30)
+        {
+            mbmicategory.setText("Obese Class I");
+            mbackground.setBackgroundColor(Color.RED);
+            mimageview.setImageResource(R.drawable.warning);
+        }
+        else
+        {
+            mbmicategory.setText("Obese Class II");
+            mbackground.setBackgroundColor(Color.RED);
+            mimageview.setImageResource(R.drawable.cross);
+        }
+
+
+        mgender.setText(intent.getStringExtra("gender"));
+        mbmidisplay.setText(mbmi);
+
+
+        getSupportActionBar().hide();
+        mrecalculatebmi = findViewById(R.id.recalculatebmi);
+
+        mrecalculatebmi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(bmiactivity.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
